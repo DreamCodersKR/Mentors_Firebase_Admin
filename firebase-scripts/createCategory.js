06160398f9fff7072e33aac90175af8531f41eaf
yoon <llsee01@naver.com>
2025-02-10 09:33:59 +0900

    카테고리 생성

--- firebase-scripts/createCategory.js
+++ firebase-scripts/createCategory.js
@@ -15,29 +15,29 @@ const firestore = admin.firestore();
 const addCategories = async () => {
   const categories = [
     {
-      cate_name: "멘토링후기",
-      what_for: "board",
+      cate_name: "공지사항",
+      what_for: "admin",
       createdDt: admin.firestore.FieldValue.serverTimestamp(),
       isDeleted: false,
     },
-    {
-      cate_name: "팁과 노하우",
-      what_for: "board",
-      createdDt: admin.firestore.FieldValue.serverTimestamp(),
-      isDeleted: false,
-    },
-    {
-      cate_name: "고민상담",
-      what_for: "board",
-      createdDt: admin.firestore.FieldValue.serverTimestamp(),
-      isDeleted: false,
-    },
-    {
-      cate_name: "자격증&시험정보",
-      what_for: "board",
-      createdDt: admin.firestore.FieldValue.serverTimestamp(),
-      isDeleted: false,
-    },
+    // {
+    //   cate_name: "팁과 노하우",
+    //   what_for: "board",
+    //   createdDt: admin.firestore.FieldValue.serverTimestamp(),
+    //   isDeleted: false,
+    // },
+    // {
+    //   cate_name: "고민상담",
+    //   what_for: "board",
+    //   createdDt: admin.firestore.FieldValue.serverTimestamp(),
+    //   isDeleted: false,
+    // },
+    // {
+    //   cate_name: "자격증&시험정보",
+    //   what_for: "board",
+    //   createdDt: admin.firestore.FieldValue.serverTimestamp(),
+    //   isDeleted: false,
+    // },
   ];
 
   try {
